1337fafff76e2fb88a945d1259b0190bd45d7368
ankit <2017araj@gmail.com>
2025-07-28 00:54:29 +0530

    minor change

--- src/main/java/com/example/notification/service/RateLimitService.java
+++ src/main/java/com/example/notification/service/RateLimitService.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 @Service
